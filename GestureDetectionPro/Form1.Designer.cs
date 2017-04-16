e06dfcfd3081b62ae44462d28bd96f38d19cdcb3
Varsha Jambunathan <jvarsha621@gmail.com>
2017-04-16 11:03:44 +0530

    Initial commit

---
+++ GestureDetectionPro/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace GestureDetectionPro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.skinColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayscaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayscaleToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.bWUsingAForgeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.largestBlobToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.processingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setGetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unlockAndMarshalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lockDMAUnsafeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parallelPlus3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recolorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.skinColorToolStripMenuItem,
+            this.filterToolStripMenuItem,
+            this.processingToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(967, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // skinColorToolStripMenuItem
+            // 
+            this.skinColorToolStripMenuItem.Name = "skinColorToolStripMenuItem";
+            this.skinColorToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.skinColorToolStripMenuItem.Text = "Skin Color";
+            this.skinColorToolStripMenuItem.Click += new System.EventHandler(this.skinColorToolStripMenuItem_Click);
+            // 
+            // filterToolStripMenuItem
+            // 
+            this.filterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grayscaleToolStripMenuItem,
+            this.grayscaleToolStripMenuItem1,
+            this.bWUsingAForgeToolStripMenuItem,
+            this.largestBlobToolStripMenuItem,
+            this.recolorToolStripMenuItem});
+            this.filterToolStripMenuItem.Name = "filterToolStripMenuItem";
+            this.filterToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.filterToolStripMenuItem.Text = "Filter";
+            // 
+            // grayscaleToolStripMenuItem
+            // 
+            this.grayscaleToolStripMenuItem.Name = "grayscaleToolStripMenuItem";
+            this.grayscaleToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.grayscaleToolStripMenuItem.Text = "Black and White";
+            this.grayscaleToolStripMenuItem.Click += new System.EventHandler(this.grayscaleToolStripMenuItem_Click);
+            // 
+            // grayscaleToolStripMenuItem1
+            // 
+            this.grayscaleToolStripMenuItem1.Name = "grayscaleToolStripMenuItem1";
+            this.grayscaleToolStripMenuItem1.Size = new System.Drawing.Size(165, 22);
+            this.grayscaleToolStripMenuItem1.Text = "Grayscale";
+            this.grayscaleToolStripMenuItem1.Click += new System.EventHandler(this.grayscaleToolStripMenuItem1_Click);
+            // 
+            // bWUsingAForgeToolStripMenuItem
+            // 
+            this.bWUsingAForgeToolStripMenuItem.Name = "bWUsingAForgeToolStripMenuItem";
+            this.bWUsingAForgeToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.bWUsingAForgeToolStripMenuItem.Text = "BW using AForge";
+            this.bWUsingAForgeToolStripMenuItem.Click += new System.EventHandler(this.bWUsingAForgeToolStripMenuItem_Click);
+            // 
+            // largestBlobToolStripMenuItem
+            // 
+            this.largestBlobToolStripMenuItem.Name = "largestBlobToolStripMenuItem";
+            this.largestBlobToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.largestBlobToolStripMenuItem.Text = "Largest Blob";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(955, 354);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(303, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(320, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(626, 28);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // processingToolStripMenuItem
+            // 
+            this.processingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setGetToolStripMenuItem,
+            this.unlockAndMarshalToolStripMenuItem,
+            this.lockDMAUnsafeToolStripMenuItem,
+            this.parallelPlus3ToolStripMenuItem});
+            this.processingToolStripMenuItem.Name = "processingToolStripMenuItem";
+            this.processingToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.processingToolStripMenuItem.Text = "Processing";
+            this.processingToolStripMenuItem.Click += new System.EventHandler(this.processingToolStripMenuItem_Click);
+            // 
+            // setGetToolStripMenuItem
+            // 
+            this.setGetToolStripMenuItem.Name = "setGetToolStripMenuItem";
+            this.setGetToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.setGetToolStripMenuItem.Text = "Set and Get";
+            // 
+            // unlockAndMarshalToolStripMenuItem
+            // 
+            this.unlockAndMarshalToolStripMenuItem.Name = "unlockAndMarshalToolStripMenuItem";
+            this.unlockAndMarshalToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.unlockAndMarshalToolStripMenuItem.Text = "Lock and marshal";
+            this.unlockAndMarshalToolStripMenuItem.Click += new System.EventHandler(this.unlockAndMarshalToolStripMenuItem_Click);
+            // 
+            // lockDMAUnsafeToolStripMenuItem
+            // 
+            this.lockDMAUnsafeToolStripMenuItem.Name = "lockDMAUnsafeToolStripMenuItem";
+            this.lockDMAUnsafeToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.lockDMAUnsafeToolStripMenuItem.Text = "Lock DMA Unsafe";
+            // 
+            // parallelPlus3ToolStripMenuItem
+            // 
+            this.parallelPlus3ToolStripMenuItem.Name = "parallelPlus3ToolStripMenuItem";
+            this.parallelPlus3ToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.parallelPlus3ToolStripMenuItem.Text = "Parallel plus 3";
+            this.parallelPlus3ToolStripMenuItem.Click += new System.EventHandler(this.parallelPlus3ToolStripMenuItem_Click);
+            // 
+            // recolorToolStripMenuItem
+            // 
+            this.recolorToolStripMenuItem.Name = "recolorToolStripMenuItem";
+            this.recolorToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.recolorToolStripMenuItem.Text = "recolor";
+            this.recolorToolStripMenuItem.Click += new System.EventHandler(this.recolorToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 382);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem skinColorToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem filterToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ToolStripMenuItem grayscaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayscaleToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem bWUsingAForgeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem largestBlobToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setGetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unlockAndMarshalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lockDMAUnsafeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parallelPlus3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recolorToolStripMenuItem;
+    }
+}
+
---
+++ GestureDetectionPro/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace GestureDetectionPro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //public class ExtractBiggestBlob 
+
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            open.InitialDirectory = @"G:\Saarthi";
+            open.Title = "Select an Image";
+
+            open.CheckFileExists = true;
+            open.CheckPathExists = true;
+
+            open.DefaultExt = "jpg";
+            open.FilterIndex = 2;
+            open.RestoreDirectory = true;
+
+            open.ReadOnlyChecked = true;
+            open.ShowReadOnly = true;
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                // Create a new Bitmap object from the picture file on disk,
+                // and assign that to the PictureBox.Image property
+                pictureBox1.Image = new Bitmap(open.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        int max(int x, int y, int z)
+        {
+            if (x > y && x > z)
+                return x;
+            else if (y > x && y > z)
+                return y;
+            else
+                return z;
+        }
+
+        int min(int x, int y, int z)
+        {
+            if (x < y && x < z)
+                return x;
+            else if (y < x && y < z)
+                return y;
+            else
+                return z;
+        }
+
+        private void grayscaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image, new Size(300,300));
+            int width = bmp.Width;
+            int height = bmp.Height;
+            int[] arr = new int[225];
+            int i = 0;
+            Color p;
+
+            //Grayscale
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    p = bmp.GetPixel(x, y);
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+                    int avg = (r + g + b) / 3;
+                    avg = avg < 128 ? 0 : 255;     // Converting gray pixels to either pure black or pure white
+                    bmp.SetPixel(x, y, Color.FromArgb(a, avg, avg, avg));
+                }
+            }
+            pictureBox3.Image = bmp;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void grayscaleToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            int width = bmp.Width;
+            int height = bmp.Height;
+            int[] arr = new int[225];
+            int i = 0;
+            Color p;
+
+            //Grayscale
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    p = bmp.GetPixel(x, y);
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+                    int avg = (r + g + b) / 3;
+                    bmp.SetPixel(x, y, Color.FromArgb(a, avg, avg, avg));
+                }
+            }
+            pictureBox3.Image = bmp;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void skinColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Extracting RGBs
+            Bitmap hand = new Bitmap(pictureBox1.Image);
+            Bitmap skindetect = new Bitmap(hand.Width, hand.Height);
+            //Bitmap blackWhite = new Bitmap(hand.Width, hand.Height);
+
+            Color black = Color.Black;
+            //Color white = Color.White;
+
+            int i, j;
+            for (i = 0; i < hand.Width; i++)
+            {
+                for (j = 0; j < hand.Height; j++)
+                {
+                    Color pixel = hand.GetPixel(i, j);
+
+                    int red = pixel.R;
+                    int green = pixel.G;
+                    int blue = pixel.B;
+
+
+
+                    /* (R, G, B) is classified as skin if: 
+                        R > 95 and G > 40 and B > 20 and 
+                        max {R, G, B} – min{R, G, B} > 15 and 
+                        |R – G| > 15 and R > G and R > B
+                    */
+                    if ((red > 95 && green > 40 && blue > 20) && (max(red, green, blue) - min(red, green, blue) > 15)
+                        && Math.Abs(red - green) > 15 && red > green && red > blue)
+                    {
+                        //Console.WriteLine("Success");
+                        skindetect.SetPixel(i, j, pixel);
+                        //blackWhite.SetPixel(i, j, white);
+                    }
+                    else
+                    {
+                        //skindetect.SetPixel(i, j, black);
+                        //blackWhite.SetPixel(i, j, black);
+                    }
+
+                }
+            }
+
+            pictureBox2.Image = new Bitmap(skindetect);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            //pictureBox3.Image = new Bitmap(blackWhite);
+            //pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void bWUsingAForgeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Grayscale filter = new Grayscale(0.2125, 0.71254, 0.0721);
+            Bitmap grayImage = filter.Apply((Bitmap)pictureBox2.Image);
+            Threshold filter2 = new Threshold(100);
+            Bitmap filtered = filter2.Apply(grayImage);
+            Closing close = new Closing();
+            close.ApplyInPlace(filtered);
+            Opening open = new Opening();
+            open.ApplyInPlace(filtered);
+            // create filter for the filtered image
+            ExtractBiggestBlob filter3 = new ExtractBiggestBlob();
+            // apply the filter
+            Bitmap biggestBlobsImage = filter3.Apply(filtered);
+            AForge.IntPoint a = filter3.BlobPosition;
+            Console.WriteLine(a);
+
+            //Biggest blob for old extracted sskin image
+            ExtractBiggestBlob filter4 = new ExtractBiggestBlob();
+            Bitmap skinBlob = new Bitmap(pictureBox2.Image);
+            //apply filter
+            Bitmap biggestSkinBlob = filter4.Apply(skinBlob);
+            pictureBox2.Image = new Bitmap(biggestSkinBlob);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            //Skin color for largest blob
+            // This mask is logically AND with original image to extract only the palm which is required for feature extraction. 
+            Bitmap one = new Bitmap(biggestSkinBlob);
+            Bitmap two = new Bitmap(biggestBlobsImage);
+
+            int i, j;
+            for (i = 0; i < two.Width; i++)
+            {
+                for (j = 0; j < two.Height; j++)
+                {
+                    Color pixelOne = one.GetPixel(i, j);
+                    Color pixelTwo = two.GetPixel(i, j);
+
+                    int redOne = pixelOne.R;
+                    int greenOne = pixelOne.G;
+                    int blueOne = pixelOne.B;
+
+                    int redTwo = pixelTwo.R;
+                    int greenTwo = pixelTwo.G;
+                    int blueTwo = pixelTwo.B;
+
+                    two.SetPixel(i, j, Color.FromArgb(redOne & redTwo, greenOne & greenTwo, blueOne & blueTwo));
+
+                }
+            }
+            pictureBox3.Image = new Bitmap(two);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void processingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void unlockAndMarshalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap processedBitmap = new Bitmap(pictureBox2.Image);
+            BitmapData bitmapData = processedBitmap.LockBits(new Rectangle(0, 0, processedBitmap.Width, processedBitmap.Height), ImageLockMode.ReadWrite, processedBitmap.PixelFormat);
+
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(processedBitmap.PixelFormat) / 8;
+            int byteCount = bitmapData.Stride * processedBitmap.Height;
+            byte[] pixels = new byte[byteCount];
+            IntPtr ptrFirstPixel = bitmapData.Scan0;
+            Marshal.Copy(ptrFirstPixel, pixels, 0, pixels.Length);
+            int heightInPixels = bitmapData.Height;
+            int widthInBytes = bitmapData.Width * bytesPerPixel;
+
+            for (int y = 0; y < heightInPixels; y++)
+            {
+                int currentLine = y * bitmapData.Stride;
+                for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                {
+                    int oldBlue = pixels[currentLine + x];
+                    int oldGreen = pixels[currentLine + x + 1];
+                    int oldRed = pixels[currentLine + x + 2];
+
+                    // calculate new pixel value
+                    pixels[currentLine + x] = (byte)oldBlue;
+                    pixels[currentLine + x + 1] = (byte)oldGreen;
+                    pixels[currentLine + x + 2] = (byte)oldRed;
+                }
+            }
+
+            // copy modified bytes back
+            Marshal.Copy(pixels, 0, ptrFirstPixel, pixels.Length);
+            processedBitmap.UnlockBits(bitmapData);
+            pictureBox3.Image = new Bitmap(processedBitmap);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+
+
+        private void parallelPlus3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap processedBitmap = new Bitmap(pictureBox2.Image);
+            /*
+             * unsafe
+            {
+                BitmapData bitmapData = processedBitmap.LockBits(new Rectangle(0, 0, processedBitmap.Width, processedBitmap.Height), ImageLockMode.ReadWrite, processedBitmap.PixelFormat);
+
+                int bytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(processedBitmap.PixelFormat) / 8;
+                int heightInPixels = bitmapData.Height;
+                int widthInBytes = bitmapData.Width * bytesPerPixel;
+                byte* PtrFirstPixel = (byte*)bitmapData.Scan0;
+
+                Parallel.For(0, heightInPixels, y =>
+                {
+                    byte* currentLine = PtrFirstPixel + (y * bitmapData.Stride);
+                    for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                    {
+                        int oldBlue = currentLine[x];
+                        int oldGreen = currentLine[x + 1];
+                        int oldRed = currentLine[x + 2];
+
+                        currentLine[x] = (byte)oldBlue;
+                        currentLine[x + 1] = (byte)oldGreen;
+                        currentLine[x + 2] = (byte)oldRed;
+                    }
+                });
+                processedBitmap.UnlockBits(bitmapData);
+            }
+            */
+        }
+
+        private void recolorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
+
